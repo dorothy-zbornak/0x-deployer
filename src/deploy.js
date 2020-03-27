0e633d581ea661edce46af3ff2266fca3168a039
Lawrence Forman <me@merklejerk.com>
2020-03-27 16:17:47 -0400

    fix verification (maybe)

--- src/deploy.js
+++ src/deploy.js
@@ -5,6 +5,7 @@ const process = require('process');
 const { artifacts: assetProxyArtifacts } = require('@0x/contracts-asset-proxy');
 const solpp = require('solpp');
 const path = require('path');
+const fs = require('fs');
 const _ = require('lodash');
 const { URLSearchParams } = require('url');
 const secrets = require('../secrets.json');
@@ -12,28 +13,33 @@ const secrets = require('../secrets.json');
 const NETWORKS = [
     'main',
     'ropsten',
-    'kovan'
+    // 'kovan'
 ];
 
 const BRIDGES_TO_DEPLOY = [
-    'KyberBridge',
-    'Eth2DaiBridge',
-    'UniswapBridge',
-    'CurveBridge',
+    // 'KyberBridge',
+    // 'Eth2DaiBridge',
+    // 'UniswapBridge',
+    // 'CurveBridge',
+    // 'DydxBridge',
+    'DexForwarderBridge',
 ];
 
 (async () => {
-    const sources = {
+    const compilerInputs = {
         ..._.zipObject(BRIDGES_TO_DEPLOY, await Promise.all(BRIDGES_TO_DEPLOY.map(
-            bridgeName => solpp.processFile(
-                require.resolve(`@0x/contracts-asset-proxy/contracts/src/bridges/${bridgeName}.sol`),
-                { noPreprocessor: true },
-            ),
+            bridgeName => {
+                const r = JSON.parse(fs.readFileSync(
+                    require.resolve(`@0x/contracts-asset-proxy/test/generated-artifacts/${bridgeName}.input.json`),
+                ));
+                return r;
+            },
         ))),
     };
     await Promise.all(NETWORKS.map(async network => {
         for (const bridgeName of BRIDGES_TO_DEPLOY) {
             const artifact = assetProxyArtifacts[bridgeName];
+            const compilerInput = compilerInputs[bridgeName];
             const bridgeContract = new FlexContract(
                 artifact.compilerOutput.abi,
                 {
@@ -52,18 +58,18 @@ const BRIDGES_TO_DEPLOY = [
                 await delay(() => verifySource({
                     network,
                     artifact,
+                    compilerInput,
                     address: r.contractAddress,
                     name: bridgeName,
-                    code: sources[bridgeName],
                 }));
             } catch (err) {
                 console.error(err);
             }
         }
     }));
-})().catch(() => process.exit(-1)).then(() => process.exit(0));
+})().catch(err => { console.error(err); process.exit(-1) }).then(() => process.exit(0));
 
-async function delay(cb, delay=90) {
+async function delay(cb, delay=20) {
     return new Promise((accept, reject) => {
         setTimeout(() => {
             try {
@@ -76,9 +82,8 @@ async function delay(cb, delay=90) {
 }
 
 async function verifySource(opts) {
-    const { network, address, name, artifact, code, libraries } = opts;
+    const { network, address, name, compilerInput, artifact, libraries } = opts;
     const apiNetworkPrefix = network === 'main' ? 'api' : `api-${network}`;
-    const compilerInput = createCompilerStandardInput(artifact, code);
     const params = new URLSearchParams();
     params.set('apikey', secrets.etherscanKey);
     params.set('module', 'contract');
@@ -86,9 +91,10 @@ async function verifySource(opts) {
     params.set('contractaddress', address);
     params.set('sourceCode', JSON.stringify(compilerInput));
     params.set('codeformat', 'solidity-standard-json-input');
-    params.set('contractname', `${Object.keys(compilerInput.sources)[0]}:${artifact.contractName}`);
+    params.set('contractname', findContractPathSpec(compilerInput.sources, artifact.contractName));
     params.set('compilerversion', artifact.compiler.version.slice(0, -3).slice(8));
     params.set('licenseType', 12);
+    // console.log(params);
     console.log(`Verifying source code for ${name.bold} on ${network} at ${address.green.bold}...`);
     const r = await fetch(
         `https://${apiNetworkPrefix}.etherscan.io/api`,
@@ -104,14 +110,10 @@ async function verifySource(opts) {
     console.log(`Successfully verified source code for ${name.bold} on ${network} at ${address.green.bold} (ref: ${result.result})!`);
 }
 
-function createCompilerStandardInput(artifact, code) {
-    return {
-        language: 'Solidity',
-        sources: {
-            [`${artifact.contractName}.sol`]: {
-                content: code,
-            },
-        },
-        settings: { ...artifact.compiler.settings, remappings: undefined },
-    };
+function findContractPathSpec(inputSources, name) {
+    for (const file of Object.keys(inputSources)) {
+        if (file.endsWith(`${name}.sol`)) {
+            return `${file}:${name}`;
+        }
+    }
 }
